922d13f4e08a67e82c0b311d2f28d0ad6c658c80
Yogesh-Janarthanan <141064069+Yogesh-Janarthanan@users.noreply.github.com>
2024-02-14 11:39:29 +0530

    Add files via upload

---
+++ ind.js
@@ -0,0 +1,7 @@
+document.getElementById('navMenu').addEventListener('mouseover', function() {
+  this.style.backgroundColor = '#555';
+});
+
+document.getElementById('navMenu').addEventListener('mouseout', function() {
+  this.style.backgroundColor = '#333';
+});
